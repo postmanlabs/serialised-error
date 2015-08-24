6f628b34cb962b1730964074850675f333aef87f
Shamasis Bhattacharya <mail@shamasis.net>
2015-08-24 22:06:01 +0530

    Added inline documentation to code

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var SerialisedError = module.exports = function (err) {
+    // If the function is called without the `new` operator, then we do it on behalf of the callee
 	if (!(this instanceof SerialisedError)) {
 		return new SerialisedError(err);
 	}
 
-	Object.keys((err instanceof Error) && err || {}).concat(['name', 'message', 'stack']).forEach(function (key) {
+    // Iterate on user-defined properties of error and mix in the default non ennumerable properties
+	(err instanceof Error) && Object.keys(err).concat(['name', 'message', 'stack']).forEach(function (key) {
 		this[key] = err[key];
 	}, this);
 };
 
+// easy JSON conversion
 SerialisedError.prototype.toJSON = function () {
 	return JSON.stringify(this);
 };
