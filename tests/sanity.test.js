62fd0969b301934df09c83c70230f9d21a042f21
Philip (flip) Kromer <flip@mrflip.com>
2022-05-19 16:44:13 -0500

    chore: bump uuid to not cause warnings

--- tests/sanity.test.js
+++ tests/sanity.test.js
@@ -1,6 +1,8 @@
 var expect = require('expect.js'),
     SerialisedError = require('../index');
 
+var GUID_V4_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
+
 describe('serialised error', function () {
     it('must extract required properties', function () {
         var error = SerialisedError(new Error('Error Name'));
@@ -26,5 +28,10 @@ describe('serialised error', function () {
 
         expect(Object.keys(error)).to.eql(['name', 'message', 'stack', 'checksum', 'id', 'timestamp', 'stacktrace',
             'extra']);
+    });
+
+    it('must add guid to the error', function () {
+        var error = SerialisedError(new Error('Error Name'), true);
+        expect(error.id).to.match(GUID_V4_RE);
     });
 });
